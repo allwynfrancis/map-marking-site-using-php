ef8e05e86ebe97662d6846c7e08354c3708b0f4e
allwynfrancis <allwynfrancis2424@gmail.com>
2020-11-16 19:52:49 +0530

    Final Commit...Think soo :smiley:

--- db.php
+++ db.php
@@ -1,23 +1,12 @@
 <?php
-$servername = "localhost:3300";
-$username = "root";
-$password = "root";
+require 'connection.php';
 
-
-  $conn = "mysql:host=$servername;dbname=LocationDb";
-  try {
-    
-    $db = new PDO($conn, $username, $password);
-    
-    $db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
-    
-    $sth = $db->query("SELECT name,address,lat,lng,contact,sunday,monday,tuesday,wednesday,thrusday,friday,saturday,city FROM locations");
+$sth = $db->query("SELECT id,name,address,lat,lng,contact,sunday,monday,tuesday,wednesday,thrusday,friday,saturday,city FROM locations");
     $locations = $sth->fetchAll();
     
     echo json_encode( $locations );
-    // echo "Connected Successfully";
-    } catch (Exception $e) {
-    echo $e->getMessage();
-    }
+
+  
 
 ?>
+
--- details-form.php
+++ details-form.php
@@ -1,21 +1,22 @@
 <?php
     require 'connection.php';
- 
-    if(isset($_POST['submit'])){ // Fetching variables of the form which travels in URL
+    
+    
+  
+   
+    // Fetching variables of the form which travels in URL
         $name = $_POST['name'];
         $contactNumber = $_POST['contactNumber'];
-       
-        if($name !=''||$contactNumber !=''){
-        //Insert Query of SQL
-        $query = ("insert into user_search(name,contactNumber) values ('$name','$contactNumber')");
-        $db->query($query);
+        $city_id = $_POST['city_id'];
+        // $city_id1 = '5';
+        if($name !='' && $contactNumber !='' && $city_id != ''){
+            //Insert Query of SQL
+            $query = ("insert into user_search(name,mobile_number,city_id) values ('$name','$contactNumber','$city_id')");
+            $db->query($query);
         
         }
         
-        }
+        
+        echo "Recorded";
+        // echo gettype($city_id1);
 ?>
-<script type="text/javascript">
-    document.write("Your Data Have been recorded Successfully!!");
-    setTimeout("window.close();", 1000);
-    
-    </script>
